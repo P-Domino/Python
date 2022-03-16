aaa1094dc3da99632b79bd91ae9fe96dbda175cd
UNR <dominik.pieprzyk@yahoo.com>
2022-03-16 06:54:51 +0000

    reworked verified chain func

--- 003.Understanding/UpdatedBlockchain.py
+++ 003.Understanding/UpdatedBlockchain.py
@@ -1,10 +1,13 @@
 #Initializing our (empty) blockchain list
+from opcode import hascompare
+
+
 genesisBlock = {
-    'previousHash': '', # entered dummy for educational purposes 
+    'previousHash': '', 
     'index': 0, 
     'transactions': []
 }
-blockchain = []
+blockchain = [genesisBlock]
 openTransactions = []
 owner = 'Dominik'
 
@@ -36,6 +39,15 @@ def addTransaction(recipient, sender = owner, amount = 1.0):
 
 def mineBlock():
     lastBlock = blockchain[-1]
+    hashedBlock = '-'.join([str(lastBlock[key]) for key in lastBlock])
+    print(hashedBlock)
+    
+    # the same as above with use of for loop 
+    # for key in lastBlock:
+    #     value = lastBlock[key]
+    #     hashedBlock = hashedBlock + str(value)
+
+    print(hashedBlock)
     block = {
         'previousHash': 'XYZ', # entered dummy for educational purposes 
         'index': len(blockchain), 
@@ -64,18 +76,21 @@ def printBlockchainElements():
         print('-'*20)
 
 def verifyChain():
-    isValid = True
     """Verify the current blockchain and return True if it's valid, false..."""
-    for blockIndex in range(len(blockchain)):
-        if blockIndex == 0:
-            # If we're checking the first block, we should skip iteration
-            continue
-        # Check the previous block (the entire one) vs the first element of the blockchain
-        elif blockchain[blockIndex][0] == blockchain[blockIndex - 1]:
-            isValid = True
-        else:
-            isValid = False
-    return isValid
+    
+    
+    # Old Logic
+    # isValid = True
+    # for blockIndex in range(len(blockchain)):
+    #     if blockIndex == 0:
+    #         # If we're checking the first block, we should skip iteration
+    #         continue
+    #     # Check the previous block (the entire one) vs the first element of the blockchain
+    #     elif blockchain[blockIndex][0] == blockchain[blockIndex - 1]:
+    #         isValid = True
+    #     else:
+    #         isValid = False
+    # return isValid
 
 
 waitingForUserInput = True
@@ -86,7 +101,8 @@ waitingForUserInput = True
 while waitingForUserInput:
     print('Please choose:')
     print('1: Add a new transaction value')
-    print('2. Output the blockchain blocks')
+    print('2. Mine a new block')
+    print('3. Output the blockchain blocks')
     print('h: Manipulate the chain')
     print('q: Quit')
     userChoice = getUserChoice()
@@ -97,6 +113,8 @@ while waitingForUserInput:
         addTransaction(recipient, amount = amount)
         print(openTransactions)
     elif userChoice == '2':
+        mineBlock()
+    elif userChoice == '3':
         printBlockchainElements()
     elif userChoice == 'h':
         # Make sure that you don't try to "hack" the blockchain if it's empty
@@ -106,10 +124,10 @@ while waitingForUserInput:
         waitingForUserInput = False
     else:
         print('Invalid input, please choose one from the list.')
-    if not verifyChain():
-        printBlockchainElements()
-        print('Invalid blockchain!')
-        break
+    # if not verifyChain():
+    #     printBlockchainElements()
+    #     print('Invalid blockchain!')
+    #     break
 else:
     print('User Left!')
 
