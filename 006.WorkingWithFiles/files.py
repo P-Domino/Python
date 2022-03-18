26e59cc3a0ea338f77ef6dc66a5a6844de97de7a
UNR <dominik.pieprzyk@yahoo.com>
2022-03-18 20:53:56 +0000

    Exploring with statement on file manipulation

--- 006.WorkingWithFiles/files.py
+++ 006.WorkingWithFiles/files.py
@@ -1,8 +1,12 @@
-# Creating new file accessible for writing
-f = open('demo.txt', mode = 'w')
-f.write('Hello from Python!')
-# Force to write information to the file befor getting the input
-f.close()
+# # Creating new file accessible for writing
+# f = open('demo.txt', mode = 'w') # w - is overwriting all the time if we want to save content of the file we need to use append - a
+# f.write('Hello from Python!')
+# # Force to write information to the file befor getting the input
+# f.close()
 
-# Getting the input
-userInput = input('Please enter input: ')
+# # Getting the input
+# userInput = input('Please enter input: ')
+
+f = open('demo.txt', mode = 'a ')
+f.write('At this content!')
+f.close()
