7a6d7bb42f58845f0ca675527c6d35771ee0f4fb
UNR <dominik.pieprzyk@yahoo.com>
2022-03-19 17:13:40 +0000

    Updated saving to file option

--- 006.WorkingWithFiles/files.py
+++ 006.WorkingWithFiles/files.py
@@ -1,25 +1,12 @@
-# # Creating new file accessible for writing
-# f = open('demo.txt', mode = 'w') # w - is overwriting all the time if we want to save content of the file we need to use append - a
-# f.write('Hello from Python!')
-# # Force to write information to the file befor getting the input
+f = open('demo.txt', mode='r')
+# f.write('Add this content!\n')
+# file_content = f.readlines()
 # f.close()
 
-# # Getting the input
-# userInput = input('Please enter input: ')
-
-# f = open('demo.txt', mode = 'a ')
-# f.write('At this content!\n') # Using line break to create multiline content
-# f.close() 
-
-# Reading one line of the document
-# f = open ('demo.txt', mode = 'r')
-# print(f.readline())
-# f.close()
-
-# using with 
-with open('demo.txt', mode= 'r') as f: 
+# for line in file_content:
+#     print(line[:-1])
+line = f.readline()
+while line:
+    print(line)
     line = f.readline()
-    while line: 
-        print(line)
-        line = f.readline()
-print('done!')
+f.close()
